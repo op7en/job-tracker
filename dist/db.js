6352aea866c90386baae9e1d20337510b9fd3ae1
Oleg <oleg0kofficial@gmail.com>
2026-03-29 01:35:29 +0300

    fix db connection

--- dist/db.js
+++ dist/db.js
@@ -2,10 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const pool = new pg_1.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
 exports.default = pool;
