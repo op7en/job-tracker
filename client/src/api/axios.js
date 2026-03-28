d9bc9ecebfddf882e14d747bf4a353f0aeb0ea0f
Oleg <oleg0kofficial@gmail.com>
2026-03-29 02:04:10 +0300

    frontend deploy

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,21 +1,20 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 const api = axios_1.default.create({
-    baseURL: "http://localhost:3000",
+  baseURL: "https://job-tracker-production-585f.up.railway.app",
 });
-// later I will change api like this below:
-// const api = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL,
-// });
+
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token && config.headers) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  const token = localStorage.getItem("token");
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 exports.default = api;
--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,13 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://job-tracker-production-585f.up.railway.app",
 });
-// later I will change api like this below:
-
-// const api = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL,
-// });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
