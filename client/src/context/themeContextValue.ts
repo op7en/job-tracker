8c0968ef45e20f5bcb03ef4e5d7ea4e040116b4d
Oleg <oleg0kofficial@gmail.com>
2026-04-29 18:07:20 +0300

    chore(client): clean up lint issues

---
+++ client/src/context/themeContextValue.ts
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export type Theme = "dark" | "light";
+
+export const ThemeContext = createContext<{
+  theme: Theme;
+  toggle: () => void;
+}>({
+  theme: "dark",
+  toggle: () => {},
+});
